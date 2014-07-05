bee1a3015a1a6349e3b354f6c66c05c1aa11dda4
Peter.Liu <liuhui998@gmail.com>
2014-07-05 15:38:36 +0800

    现在可以上传图片了
    把 Food里的图片字段 mount 到新建的上传器 : FoodImageUploader ，并且修改一下编辑器所用的 html 控件

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,6 @@ class Food < ActiveRecord::Base
   validates :name, presence: true
   validates :price, presence: true, numericality: {greater_than_or_equal_to: 0, less_than: 100}
   validates :image, presence: true
+  
+  mount_uploader :image, FoodImageUploader
 end
