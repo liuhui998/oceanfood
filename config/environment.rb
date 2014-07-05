626c18e23a6b7ff7dc01924a7a92d44c5cde09ef
Peter.Liu <liuhui998@gmail.com>
2014-07-05 18:14:44 +0800

    为测试环境做了一些配置

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 # Initialize the Rails application.
 OceanFood::Application.initialize!
