277fc1207b752a8ed00b1c0c84f29120db6db84c
Peter.Liu <liuhui998@gmail.com>
2014-07-05 14:27:22 +0800

    proj init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OceanFood::Application.config.secret_key_base = '9e0ab45856af72aa58e06e08e7adc9ba5531ffb565a07c4eafcee41d5d6feeb1871bdee8bf83c8980b26d68c086290ecc05d4c831326613f50d8d73dde172862'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OceanFood::Application.config.session_store :cookie_store, key: '_OceanFood_session'
