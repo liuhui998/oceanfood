52b5aee8b9b4562c7fd475fb6c3489df2529f4fd
Peter.Liu <liuhui998@gmail.com>
2014-07-05 15:16:52 +0800

    验证价格不能超过 100, 也不能小于 0
    在 rails console 里执行下面的命令：
    reload!
    f = Food.new
    f.save!
    
    会出现下面的异常：
    ActiveRecord::RecordInvalid: Validation failed: Name can't be blank, Price can't be blank, Price is not a number, Image can't be blank

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ActiveRecord::Base
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 0, less_than: 100}
   validates :image, presence: true
 end
